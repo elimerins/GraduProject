b534dd6789618bd03b8232ffdd367ca0c4d9bcd5
Sangsu Lee <ssleesn@gmail.com>
2018-06-11 18:25:52 +0900

    Initial commit

---
+++ src/main/java/com/example/HomeController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
